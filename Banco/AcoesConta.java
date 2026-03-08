40c1f2cf65b67b363dc1b5f4f2c89a35a0e90d18
Leandro Davi Borges Costa <leandrodavicontato119@gmail.com>
2026-03-08 16:22:03 -0300

    feat

---
+++ Banco/AcoesConta.java
@@ -0,0 +1,12 @@
+package com.mycompany.sistema_bancario;
+public interface AcoesConta {
+    public void deposito();
+    public void saque();
+    public void transferir();
+    public void consultaSaldo();
+    public void gerarRendimentos();
+    public void pagImposto();
+    public void cadastrarCliente();
+    public void abrirConta();
+    public void fecharConta();
+}
